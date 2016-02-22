8070680e44895863e5d1981d609f3e914ae5d253
Sam Alfred <sam.mike.alfred@gmail.com>
2016-02-22 21:45:43 +0200

    Finished map

---
+++ Assets/_Scripts/ToggleSunlight.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToggleSunlight : MonoBehaviour {
+
+    public GameObject sunlight;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKeyDown(KeyCode.L))
+        {
+            sunlight.SetActive(!sunlight.activeInHierarchy);
+            RenderSettings.fog = !sunlight.activeInHierarchy;
+        }
+	}
+}
