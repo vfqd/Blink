de571f63a25056b338ef9fdab93fb2d89a9773a9
Sam Alfred <sam.mike.alfred@gmail.com>
2016-03-08 22:33:18 +0200

    Added a static effect when the monster is near

---
+++ Assets/Scripts/AssignNotesScript.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AssignNotesScript : MonoBehaviour {
+
+    [SerializeField] private GameObject[] noteLocations;
+    [SerializeField] private Material[] materials;
+
+    bool notesPopulated = false;
+	
+	// Update is called once per frame
+	void Update () {
+        if (!notesPopulated)
+        {
+            for (int i = 0; i < noteLocations.Length; i++)
+            {
+                if (Vector3.Distance(this.transform.position, noteLocations[i].transform.position) < 15)
+                {
+                    PopulateNotes(i);
+                    Debug.Log("Populating Notes");
+                    notesPopulated = true;
+                }
+            }
+        }
+	}
+
+    void PopulateNotes(int nearNote)
+    {
+        noteLocations[nearNote].GetComponent<Renderer>().material = materials[4];
+
+        int m = 0;
+        for (int i = 0; i < noteLocations.Length; i++)
+        {
+            if (i != nearNote)
+            {
+                noteLocations[i].GetComponent<Renderer>().material = materials[m];
+                m++;
+            }
+            
+        }
+    }
+}
---
+++ Assets/Scripts/HealthAndStatic.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthAndStatic : MonoBehaviour {
+
+    public float health = 100f;
+    private float startingHealth;
+    [SerializeField] private float healthDecayRate = 5f;
+    [SerializeField] private Renderer staticRenderer;
+    [SerializeField] private GameObject monster;
+
+    // Use this for initialization
+    void Start () {
+        startingHealth = health;
+        staticRenderer.material.color = new Color(staticRenderer.material.color.r,
+                                                  staticRenderer.material.color.g,
+                                                  staticRenderer.material.color.b,
+                                                  0f);
+    }
+	
+    void Update()
+    {
+        if (Vector3.Distance(monster.transform.position,this.transform.position) < 10)
+        {
+            DecreaseHealth();
+            Debug.Log(health);
+        } else
+        {
+            IncreaseHealth();
+        }
+        OffsetTexture();
+    }
+
+    void IncreaseHealth()
+    {
+        float growthModifier = startingHealth / healthDecayRate;
+        health += growthModifier * Time.deltaTime;
+
+        float newAlpha = 1f - (health / startingHealth);
+        staticRenderer.material.color = new Color(staticRenderer.material.color.r,
+                                                  staticRenderer.material.color.g,
+                                                  staticRenderer.material.color.b,
+                                                  newAlpha);
+
+        if (health >= startingHealth)
+        {
+            health = startingHealth;
+        }
+    }
+
+	void DecreaseHealth () {
+        float decayModifier = startingHealth / healthDecayRate;
+
+        health -= decayModifier * Time.deltaTime;
+
+        float newAlpha = 1f - (health / startingHealth);
+        staticRenderer.material.color = new Color(staticRenderer.material.color.r,
+                                                  staticRenderer.material.color.g,
+                                                  staticRenderer.material.color.b,
+                                                  newAlpha);
+
+        if (health <= 0)
+        {
+            Debug.Log("Player out of health");
+
+            //LOSE CONDITION
+        }
+	}
+
+    void OffsetTexture()
+    {
+        float xOffset = Random.value;
+        float yOffset = Random.value;
+
+        staticRenderer.material.mainTextureOffset = new Vector2(xOffset, yOffset);
+    }
+}
